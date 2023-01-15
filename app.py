c49e9456e9c03f9d9a679bef595bfb0fde3aeedb
samau3 <samau415@gmail.com>
2023-01-15 12:43:01 -0800

    Add preping config to keep DB connection active

--- app.py
+++ app.py
@@ -13,6 +13,9 @@ app.config['SQLALCHEMY_DATABASE_URI'] = (
     os.environ['DATABASE_URL'].replace("postgres://", "postgresql://"))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
+# need below line to keep db connection to avoid the following error:
+# sqlalchemy.exc.OperationalError: (psycopg2.OperationalError) server closed the connection unexpectedly
+app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {"pool_pre_ping": True}
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
 
