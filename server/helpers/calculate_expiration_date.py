3765ff850a87314b43b5c13d2e7389b1d8ab26bd
samau3 <samau415@gmail.com>
2023-08-20 15:56:39 -0700

    Separate utcnow to mock more easily

--- server/helpers/calculate_expiration_date.py
+++ server/helpers/calculate_expiration_date.py
@@ -1,6 +1,17 @@
 from datetime import datetime, timedelta
 
 
-def calculate_expiration_date(number_of_days):
-    valid_until = datetime.utcnow() + timedelta(days=number_of_days)
+class DateTimeProvider:  # Add DateTimeProvider to enable easier testing
+    @staticmethod
+    def get_utc_now():
+        """Returns current UTC timestamp"""
+        return datetime.utcnow()
+
+
+def calculate_expiration_date(number_of_days, datetime_provider=None):
+    """Calculates a date X days from date of function call"""
+    if datetime_provider is None:
+        datetime_provider = DateTimeProvider()
+
+    valid_until = datetime_provider.get_utc_now() + timedelta(days=number_of_days)
     return valid_until
