8c236275408487530185efbb51c1bfed964a6807
samau3 <samau415@gmail.com>
2023-08-20 15:56:51 -0700

    Add test for calculating expiration date

--- server/tests.py
+++ server/tests.py
@@ -1,6 +1,25 @@
 from unittest import TestCase, main
 from unittest.mock import patch, Mock
+from datetime import datetime, timedelta
+
 from main import check_items
+from helpers.calculate_expiration_date import calculate_expiration_date, DateTimeProvider
+
+
+class TestCalculateExpDate(TestCase):
+    def test_calculate_expiration_date(self):
+        number_of_days = 7
+        # Choose a fixed datetime for testing
+        fixed_datetime = datetime(2023, 8, 20, 12, 0, 0)
+
+        # Mock DateTimeProvider to return the fixed_datetime
+        datetime_provider = Mock(spec=DateTimeProvider)
+        datetime_provider.get_utc_now.return_value = fixed_datetime
+
+        expected_result = fixed_datetime + timedelta(days=number_of_days)
+
+        result = calculate_expiration_date(number_of_days, datetime_provider)
+        self.assertEqual(result, expected_result)
 
 
 class TestCheckItemsFunction(TestCase):
@@ -39,7 +58,6 @@ class TestCheckItemsFunction(TestCase):
 
         result = check_items()
         self.assertEqual(result, expected_result)
-
 
     @patch('main.requests.get')  # Mocking the requests.get function
     def test_check_items_no_stock(self, mock_get):
