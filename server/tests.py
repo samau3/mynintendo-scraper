9aa8b9c95e88a372994b223465255557c1cf0e50
samau3 <samau415@gmail.com>
2023-08-20 14:59:39 -0700

    Add out of stock test for check_items

--- server/tests.py
+++ server/tests.py
@@ -6,7 +6,7 @@ from main import check_items
 class TestCheckItemsFunction(TestCase):
 
     @patch('main.requests.get')  # Mocking the requests.get function
-    def test_check_items(self, mock_get):
+    def test_check_items_in_stock(self, mock_get):
         # Mocked HTML content for testing
         mock_html_text = """
             <div class="BasicTilestyles__Info-sc">
@@ -35,6 +35,42 @@ class TestCheckItemsFunction(TestCase):
 
         expected_result = {
             "Item 1": "$10"
+        }
+
+        result = check_items()
+        self.assertEqual(result, expected_result)
+
+
+    @patch('main.requests.get')  # Mocking the requests.get function
+    def test_check_items_no_stock(self, mock_get):
+        # Mocked HTML content for testing
+        mock_html_text = """
+            <div class="BasicTilestyles__Info-sc">
+                <div>
+                    <h2>Item 1</h2>
+                </div>
+                <div class="ProductTilestyles__DescriptionTag-sc">Out of Stock</div>
+                <div class="ProductTilestyles__PriceWrapper-sc">
+                    <div>
+                        <div>
+                            <span>
+                                <div>
+                                    <span>$10</span>
+                                </div>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        """
+
+        # Create a mock response object and configure its behavior
+        mock_response = Mock()
+        mock_response.text = mock_html_text
+        mock_get.return_value = mock_response
+
+        expected_result = {
+            "Item 1": "Out of Stock"
         }
 
         result = check_items()
