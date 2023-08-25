964b264dd36b51f3d8eacdecaeca8e28d158da9b
samau3 <samau415@gmail.com>
2023-08-25 14:38:42 -0700

    Add error classes

---
+++ server/errors.py
@@ -0,0 +1,15 @@
+class DatabaseError(Exception):
+    """Custom exception for database-related errors."""
+    def __init__(self, message, original_exception=None):
+        super().__init__(message)
+        self.original_exception = original_exception
+
+class CSSTagSelectorError(Exception):
+    """Custom exception for database-related errors."""
+    def __init__(self, message, original_exception=None):
+        super().__init__(message)
+        self.original_exception = original_exception
+
+class CustomError(Exception):
+    def __init__(self, message):
+        self.message = message
