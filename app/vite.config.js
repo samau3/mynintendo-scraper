960c11f0ddf01a25afc9ab80639aa10453d07c8f
samau3 <samau415@gmail.com>
2023-10-09 16:57:23 -0700

    Add Typerscript checking to Vite

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,10 +1,15 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
-
+import checker from 'vite-plugin-checker'
 
 export default defineConfig(() => {
   return {
-    plugins: [react()],
+    plugins: [
+      react(),
+      checker({
+        typescript: true,
+      }),
+    ],
     server: {
       open: true,
       host: true,
