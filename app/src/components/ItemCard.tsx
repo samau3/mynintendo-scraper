db8c02e8dc30d5a9f9e53a8648eaf4f999afd904
samau3 <samau415@gmail.com>
2024-03-28 15:54:02 -0700

    Hide image for now and increase price font weight

--- app/src/components/ItemCard.tsx
+++ app/src/components/ItemCard.tsx
@@ -10,11 +10,11 @@ export function ItemCard({ item, index, cost }: ItemCardProps) {
       key={`${item}-${index}`}
       className="w-full h-full flex flex-col shadow-lg p-4 bg-white dark:bg-slate-800 rounded-lg justify-between"
     >
-      <img src="https://placehold.co/300" />
+      {/* <img src="https://placehold.co/300" /> */}
       <p className="mb-auto text-lg tracking-tight text-black dark:text-slate-200 ">
         {item}
       </p>
-      <p className="text-md tracking-tighter text-black dark:text-slate-300 ">
+      <p className="text-md font-medium tracking-tighter text-black dark:text-slate-300 ">
         {cost}
       </p>
     </div>
