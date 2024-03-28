82dace61c990823968b1891035363e1f7bc75cf9
samau3 <samau415@gmail.com>
2024-03-28 15:43:58 -0700

    Properly change appearance of divs in dark mode

--- app/src/components/Changes.tsx
+++ app/src/components/Changes.tsx
@@ -9,19 +9,19 @@ interface ChangesProps {
 export default function Changes({ recentChange, lastChange }: ChangesProps) {
   return (
     <div className="flex gap-x-3 mb-3">
-      <div className="flex-1 bg-white rounded-md p-2 ">
-        <p>Latest Scrape Results:</p>
+      <div className="flex-1 bg-white rounded-md p-2 dark:bg-slate-800 ">
+        <p className="dark:text-slate-300">Latest Scrape Results:</p>
         {typeof recentChange.items === "string" ? (
-          <p className="font-bold text-lg"> {recentChange.items}</p>
+          <p className="font-bold text-lg dark:text-slate-300"> {recentChange.items}</p>
         ) : (
           <ChangedItemsList changedItemsData={recentChange.items} />
         )}
       </div>
-      <div className="flex-1 bg-white rounded-md p-2 ">
-        <p>Last Change:</p>
+      <div className="flex-1 bg-white rounded-md p-2 dark:bg-slate-800 ">
+        <p className="dark:text-slate-300">Last Change:</p>
 
         <ChangedItemsList changedItemsData={lastChange.items} />
-        <p className="font-bold">
+        <p className="font-bold dark:text-slate-300">
           From {new Date(lastChange.timestamp).toLocaleString()}
         </p>
       </div>
