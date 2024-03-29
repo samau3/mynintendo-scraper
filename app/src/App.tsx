274ca253d018ceab05355b38e1a84ea09eb6b12d
samau3 <samau415@gmail.com>
2024-03-29 14:55:49 -0700

    Allow Items div to take up full window width

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -15,8 +15,7 @@ function App() {
           <div id={styles["stripe-pattern"]}></div>
         </div>
       </div>
-
-      <div className="flex flex-col items-center p-4 min-h-screen absolute top-0 z-10">
+      <div className="flex flex-col items-center p-4 min-h-screen min-w-full absolute top-0 z-10 ">
         <div className="w-full flex flex-row-reverse sm:absolute sm:w-min right-5 top-5">
           <ThemeSwitcher />
         </div>
