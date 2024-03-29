7882d2e4deb0439e14a4c209d92533b79775f326
samau3 <samau415@gmail.com>
2024-03-29 12:41:14 -0700

    Add Mario UI

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,14 +1,28 @@
 import Items from "./components/Items";
 import ThemeSwitcher from "./components/ThemeSwitcher";
 
+import styles from "./App.module.css";
+
 function App() {
   return (
-    <div className="flex flex-col items-center p-4 dark:bg-slate-800 min-h-screen">
-      <div className="w-full flex flex-row-reverse sm:absolute sm:w-min right-5 top-5" >
-        <ThemeSwitcher />
+    <>
+      <div id={styles.app}>
+        <div id={styles["star-container"]}>
+          <div id={styles["star-pattern"]}></div>
+          <div id={styles["star-gradient-overlay"]}></div>
+        </div>
+        <div id={styles["stripe-container"]}>
+          <div id={styles["stripe-pattern"]}></div>
+        </div>
+
+        {/* <div className="flex flex-col items-center p-4 min-h-screen">
+          <div className="w-full flex flex-row-reverse sm:absolute sm:w-min right-5 top-5">
+            <ThemeSwitcher />
+          </div>
+          <Items />
+        </div> */}
       </div>
-      <Items />
-    </div>
+    </>
   );
 }
 
