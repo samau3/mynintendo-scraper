992c33c6dc52508b4f563bc6b2272c18a91b2feb
samau3 <samau415@gmail.com>
2024-03-29 13:06:23 -0700

    Allowed pattern to be fixed as a background

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -6,7 +6,7 @@ import styles from "./App.module.css";
 function App() {
   return (
     <>
-      <div id={styles.app}>
+      <div id={styles.wrapper}>
         <div id={styles["star-container"]}>
           <div id={styles["star-pattern"]}></div>
           <div id={styles["star-gradient-overlay"]}></div>
@@ -14,13 +14,13 @@ function App() {
         <div id={styles["stripe-container"]}>
           <div id={styles["stripe-pattern"]}></div>
         </div>
+      </div>
 
-        {/* <div className="flex flex-col items-center p-4 min-h-screen">
-          <div className="w-full flex flex-row-reverse sm:absolute sm:w-min right-5 top-5">
-            <ThemeSwitcher />
-          </div>
-          <Items />
-        </div> */}
+      <div className="flex flex-col items-center p-4 min-h-screen absolute top-0 z-10">
+        <div className="w-full flex flex-row-reverse sm:absolute sm:w-min right-5 top-5">
+          <ThemeSwitcher />
+        </div>
+        <Items />
       </div>
     </>
   );
