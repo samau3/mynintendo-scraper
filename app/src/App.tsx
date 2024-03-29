a0a99ec2516c59937008cbf9c25e7d9d802fe49a
samau3 <samau415@gmail.com>
2024-03-28 18:08:44 -0700

    Remove unnecessary bg white

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -3,8 +3,8 @@ import ThemeSwitcher from "./components/ThemeSwitcher";
 
 function App() {
   return (
-    <div className="flex flex-col items-center p-4 bg-white dark:bg-slate-800 min-h-screen">
-      <div className="w-full flex flex-row-reverse sm:absolute sm:w-min right-5 top-5">
+    <div className="flex flex-col items-center p-4 dark:bg-slate-800 min-h-screen">
+      <div className="w-full flex flex-row-reverse sm:absolute sm:w-min right-5 top-5" >
         <ThemeSwitcher />
       </div>
       <Items />
