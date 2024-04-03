35d0dbf37d0c5f8e9795d3b9522b3c82314a6236
samau3 <samau415@gmail.com>
2024-04-02 17:35:43 -0700

    Add api status page

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -20,6 +20,14 @@ function App() {
           <ThemeSwitcher />
         </div>
         <Items />
+        <button className="uppercase text-sm font-bold text-blue-600 p-2 m-2 bg-blue-100 rounded-lg">
+              <a
+                href="https://j50pzswk.status.cron-job.org/"
+                target="_blank"
+              >
+                API Status
+              </a>
+            </button>
       </div>
     </>
   );
