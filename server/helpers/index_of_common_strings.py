5802400cf702253c2a55d5e13e656429fc91324c
samau3 <samau415@gmail.com>
2024-04-03 00:46:05 -0700

    Remove old comment

--- server/helpers/index_of_common_strings.py
+++ server/helpers/index_of_common_strings.py
@@ -1,7 +1,7 @@
 def index_of_common_strings(list1, list2):
     list1_indices = []
     list2_indices = []
-    for idx, item in enumerate(list1):  # Iterate over a copy of list1 to avoid modifying it while iterating
+    for idx, item in enumerate(list1):
         if item in list2:
             list1_indices.append(idx)
             list2_indices.append(list2.index(item))
