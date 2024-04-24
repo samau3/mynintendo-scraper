dc82b7c6d8c8c2223a8bc70bd15dc39f600f21a4
samau3 <samau415@gmail.com>
2024-04-23 17:21:56 -0700

    Add headless mode to selenium

--- server/helpers/load_page_data.py
+++ server/helpers/load_page_data.py
@@ -5,10 +5,16 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 
+options = webdriver.ChromeOptions()
+options.add_argument('--headless=new')
+
 def load_page_data(url, tag):
     """ Function to load a webpage and wait for a specific tag to load"""
-    driver=webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+    driver = webdriver.Chrome(service=Service(
+        ChromeDriverManager().install()), options=options)
     driver.get(url)
-    WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.CLASS_NAME, tag)))
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_all_elements_located((By.CLASS_NAME, tag)))
 
     return driver.page_source
