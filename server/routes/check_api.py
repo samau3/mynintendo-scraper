864d5a991f69cf0b0f0858bc90632b64bde37519
samau3 <samau415@gmail.com>
2024-07-03 17:06:51 -0700

    Add docstrings to route fns

--- server/routes/check_api.py
+++ server/routes/check_api.py
@@ -4,8 +4,16 @@ from errors import CustomError
 
 check_api = Blueprint("check", __name__)
 
+
 @check_api.route('/check-fly')
 def check_fly():
+    """
+    Check if the 'fly' component of the API is running.
+
+    Returns:
+        A JSON response indicating that the 'fly' component is running.
+        Raises a CustomError if there is an issue with the 'fly' component.
+    """
     try:
         return jsonify("API is running.")
     except Exception as err:
@@ -15,6 +23,13 @@ def check_fly():
 
 @check_api.route("/check-scraping")
 def check_scraping():
+    """
+    Check if the scraping functionality of the API is running.
+
+    Returns:
+        A JSON response indicating that the scraping functionality is running.
+        Raises a CustomError if the scraping function fails.
+    """
     try:
         raw_item_elements = load_items()
         get_items(raw_item_elements)
@@ -26,6 +41,13 @@ def check_scraping():
 
 @check_api.route("/check-db")
 def check_fly_db():
+    """
+    Check if the database functionality of the API is running.
+
+    Returns:
+        A JSON response indicating that the database functionality is running.
+        Raises a CustomError if the database function fails.
+    """
     try:
         get_changes()
         return jsonify("API DB is running.")
--- server/routes/main_api.py
+++ server/routes/main_api.py
@@ -3,8 +3,16 @@ from main import scrape_mynintendo, message_discord, delete_old_records, get_ite
 
 main_api = Blueprint("main", __name__)
 
+
 @main_api.get('/scrape')
 def call_scrape_fn():
+    """
+    Scrapes items and checks for changes. If changes are found, notifies via Discord.
+
+    Returns:
+        A JSON response containing the timestamp of the scrape.
+    """
+
     raw_item_elements = load_items()
     items = get_items(raw_item_elements)
     results = scrape_mynintendo(items)
@@ -17,6 +25,12 @@ def call_scrape_fn():
 
 @main_api.get('/delete')
 def delete_records():
+    """
+    Deletes old records from the database.
+
+    Returns:
+        A string indicating the number of deleted entries.
+    """
 
     results = delete_old_records()
     return f"Deleted {results} entries."
@@ -24,6 +38,13 @@ def delete_records():
 
 @main_api.get("/get-items")
 def call_get_items():
+    """
+    Retrieves and processes items.
+
+    Returns:
+        A JSON response containing the processed items.
+    """
+
     raw_item_elements = load_items()
     items = get_items(raw_item_elements)
 
