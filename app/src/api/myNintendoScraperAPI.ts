dd54c07394963be52a6b6a03a632daaef9507c00
samau3 <samau415@gmail.com>
2024-08-21 13:47:43 -0700

    Replace axios with fetch

--- app/src/api/myNintendoScraperAPI.ts
+++ app/src/api/myNintendoScraperAPI.ts
@@ -1,11 +1,9 @@
-import axios from "axios";
-
 import { BACKEND_BASE_URL } from "../config";
 
 export class MyNintendoScraperAPI {
   static async getItems() {
-    const response = (await axios({ method: "GET", url: BACKEND_BASE_URL }))
-      .data;
+    const rawResponse = await fetch(BACKEND_BASE_URL);
+    const response = await rawResponse.json();
 
     return response;
   }
