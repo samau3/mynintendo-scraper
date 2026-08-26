53a1e1d1743044c19a577fd53f8dc0af81128d17
samau3 <samau415@gmail.com>
2026-08-25 17:07:22 -0700

    feat: allow changed item containers to be scrollable

--- app/src/components/ChangedItemsList.tsx
+++ app/src/components/ChangedItemsList.tsx
@@ -7,7 +7,7 @@ interface ChangedItemsListProps {
 
 export function ChangedItemsList({ changedItemsData }: ChangedItemsListProps) {
   return (
-    <>
+    <div className="max-h-80 overflow-y-auto">
       {Object.keys(changedItemsData).map((changeCategory, index) => (
         <div key={index}>
           <div>
@@ -30,6 +30,6 @@ export function ChangedItemsList({ changedItemsData }: ChangedItemsListProps) {
           </div>
         </div>
       ))}
-    </>
+    </div>
   );
 }
